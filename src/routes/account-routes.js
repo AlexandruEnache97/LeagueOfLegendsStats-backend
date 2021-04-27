cadfdd91b441ced7f92fbdc07748c086d1675097
unknown <alexenache43@yahoo.com>
2021-04-27 22:24:54 +0300

    update-key

--- src/routes/account-routes.js
+++ src/routes/account-routes.js
@@ -7,7 +7,7 @@ module.exports = (app) => {
     app.use(cors());
 
     app.get(`${serverConfig.BASE_URL}/getUser/:username`, cors(), async (req, res) => {
-        const response = await fetch(`https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${req.params.username}?api_key=RGAPI-02bb9f9c-79c5-4a78-8127-af022712aa0f`, cors())
+        const response = await fetch(`https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${req.params.username}?api_key=RGAPI-4122ada6-4034-4633-9a37-4e48af3db69c`, cors())
         const data = await response.json();
 
         res.status(200).json(data);
